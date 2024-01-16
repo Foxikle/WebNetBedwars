84b1d8ba43fe8f950b9d9c20fd67318c14683a86
Webhead1104 <minecraft1234new@hotmail.com>
2024-01-16 10:03:32 -0600

    removing more lines :)

--- src/main/java/dev/foxikle/webnetbedwars/ItemAbilityDispatcher.java
+++ src/main/java/dev/foxikle/webnetbedwars/ItemAbilityDispatcher.java
@@ -1,16 +1,10 @@
 package dev.foxikle.webnetbedwars;
 
-import dev.foxikle.webnetbedwars.utils.Items;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
-
-import static org.bukkit.Bukkit.getPlayer;
 
 public class ItemAbilityDispatcher {
     private final WebNetBedWars plugin;
